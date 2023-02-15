7a008306ed13292c2cbbb8a031d02a6f4620f4fb
Jacob Maier <jacobamaier@gmail.com>
2023-02-15 09:15:56 -0800

    Separate logic with refactor.
    Co-authored-by: Gantz <mgantz22@icloud.com>
    Co-authored-by: Carl Sustrich <CarlSustrich@users.noreply.github.com>
    Co-authored-by: jacksonlevine <jacksonlevine@users.noreply.github.com>
    Co-authored-by: Razieleron <Razieleron@users.noreply.github.com>
    Co-authored-by: Ashe Urban <AsheUrban@users.noreply.github.com>

---
+++ src/js/enemy.js
@@ -0,0 +1,62 @@
+export class Enemy {
+  constructor(game) {
+    this.game = game;
+    this.x = this.game.width;
+    this.speedX = Math.random() * -1.5 - 0.5;
+    this.markedForDeletion = false;
+  }
+  update() {
+    this.x += this.speedX;
+    if (this.x + this.width < 0) this.markedForDeletion = true;
+  }
+  draw(context) {
+    context.fillStyle = 'orange';
+    context.fillRect(this.x, this.y, this.width, this.height);
+    context.fillStyle = 'black';
+    context.font = '20px Papyrus';
+    context.fillText(this.lives, this.x + (this.width / 2), this.y + (this.height / 2));
+  }
+}
+
+export class Angler1 extends Enemy {
+  constructor(game) {
+    super(game);
+    this.width = 100 * .5;
+    this.height = 100 * .5;
+    this.y = Math.random() * (this.game.height * 0.9 - this.height);
+    this.lives = 5;
+    this.score = this.lives;
+    //character animation
+    //this.image = document.getElementById('angler1');
+    //this.frameY = Math.floor(Math.random()* 3);
+  }
+}
+
+export class Angler2 extends Enemy {
+  constructor(game) {
+    super(game);
+    this.width = 100 * .75;
+    this.height = 100 * .75;
+    this.y = Math.random() * (this.game.height * 0.9 - this.height);
+    this.lives = 10;
+    this.score = this.lives;
+    //character animation
+    //this.image = document.getElementById('angler2');
+    //this.frameY = Math.floor(Math.random()* 3);
+  }
+}
+
+export class LuckyFish extends Enemy {
+  constructor(game) {
+    super(game);
+    this.width = 100 * .25;
+    this.height = 100 * .25;
+    this.y = Math.random() * (this.game.height * 0.9 - this.height);
+    this.lives = 3;
+    this.score = 15;
+    this.type = 'lucky';
+    //character animation
+    //this.image = document.getElementById('lucky');
+    //this.frameY = Math.floor(Math.random()* 3);
+  }
+}
---
+++ src/js/particle.js
@@ -0,0 +1,41 @@
+export class Particle {
+  constructor(game, x, y) {
+    this.game = game;
+    this.x = x;
+    this.y = y;
+    this.image = document.getElementById("particles");
+    this.frameX = Math.floor(Math.random() * 3);
+    this.frameY = Math.floor(Math.random() * 3);
+    this.spriteSize = 50;
+    this.sizeModifier = (Math.random() * 0.5 + 0.5).toFixed(1);
+    this.size = this.spriteSize * this.sizeModifier;
+    this.speedX = Math.random() * 12 - 6;
+    this.speedY = Math.random() * -15;
+    this.gravity = 0.5;
+    this.markedForDeletion = false;
+    this.angle = 0;
+    this.va = Math.random() * 0.2 - 0.1;
+    this.bounced = 0;
+    this.bottomBounceBoundary = Math.random() * 100 + 60;
+  }
+  update() {
+    this.angle += this.va;
+    this.speedY += this.gravity;
+    //move particles forward with speed
+    // this.x -= this.speedX + this.game.speed;
+    this.x -= this.speedX;
+    this.y += this.speedY;
+    if(this.y > this.game.height + this.size || this.x < 0 - this.size) this.markedForDeletion = true;
+    if (this.y > this.game.height - this.bottomBounceBoundary && this.bounced < 2) {
+      this.bounced++;
+      this.speedY *= -0.7;
+    }
+  }
+  draw(context) {
+    context.save();
+    context.translate(this.x, this.y);
+    context.rotate(this.angle);
+    context.drawImage(this.image, this.frameX * this.spriteSize, this.frameY * this.spriteSize, this.spriteSize, this.spriteSize, this.size * -0.5, this.size * -0.5, this.size, this.size);
+    context.restore();
+  }
+}
